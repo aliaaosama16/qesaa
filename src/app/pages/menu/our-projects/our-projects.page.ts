f418f595b0bc74d2fcded44ac5e44a585f941307
aliaaosama23 <engaliaaosama@gmail.com>
2022-05-04 01:48:41 +0200

    about and rules page

--- src/app/pages/menu/our-projects/our-projects.page.ts
+++ src/app/pages/menu/our-projects/our-projects.page.ts
@@ -6,7 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./our-projects.page.scss'],
 })
 export class OurProjectsPage implements OnInit {
-
+  projects: any = [
+    {
+      id: 1,
+      title: 'عنوان المشروع',
+      image:'./../../../../assets/images/projects/img1.svg'
+    },
+    {
+      id: 2,
+      title: 'عنوان المشروع',
+      image:'./../../../../assets/images/projects/img2.svg'
+    },
+    {
+      id: 3,
+      title: 'عنوان المشروع',
+      image:'./../../../../assets/images/projects/img3.svg'
+    },
+    {
+      id: 4,
+      title: 'عنوان المشروع',
+      image:'./../../../../assets/images/projects/img4.svg'
+    },
+  ];
   constructor() { }
 
   ngOnInit() {
