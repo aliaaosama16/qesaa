c2a45ac195a960eed39eac3244ed6c991ae33542
aliaaosama23 <engaliaaosama@gmail.com>
2022-05-04 01:02:11 +0200

    services page

--- src/app/pages/menu/our-services/our-services.page.ts
+++ src/app/pages/menu/our-services/our-services.page.ts
@@ -6,10 +6,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./our-services.page.scss'],
 })
 export class OurServicesPage implements OnInit {
+  services: any = [
+    {
+      id: 1,
+      title: 'عنوان الخدمة',
+      icon:'./../../../../assets/icon/services/icon1.svg'
+    },
+    {
+      id: 2,
+      title: 'عنوان الخدمة',
+      icon:'./../../../../assets/icon/services/icon2.svg'
+    },
+    {
+      id: 3,
+      title: 'عنوان الخدمة',
+      icon:'./../../../../assets/icon/services/icon3.svg'
+    },
+    {
+      id: 4,
+      title: 'عنوان الخدمة',
+      icon:'./../../../../assets/icon/services/icon1.svg'
+    },
+    {
+      id: 5,
+      title: 'عنوان الخدمة',
+      icon:'./../../../../assets/icon/services/icon2.svg'
+    },
+    {
+      id: 5,
+      title: 'عنوان الخدمة',
+      icon:'./../../../../assets/icon/services/icon1.svg'
+    },
+    {
+      id: 6,
+      title: 'عنوان الخدمة',
+      icon:'./../../../../assets/icon/services/icon3.svg'
+    },
+    {
+      id: 7,
+      title: 'عنوان الخدمة',
+      icon:'./../../../../assets/icon/services/icon2.svg'
+    },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
