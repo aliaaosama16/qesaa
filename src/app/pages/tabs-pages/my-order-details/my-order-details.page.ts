cd99e86011f10e9b7ae8430f99c14049ae6b4600
akram yasser <akramkoyasser@gmail.com>
2022-05-09 10:28:17 +0200

    order details

--- src/app/pages/tabs-pages/my-order-details/my-order-details.page.ts
+++ src/app/pages/tabs-pages/my-order-details/my-order-details.page.ts
@@ -1,3 +1,5 @@
+import { LanguageService } from 'src/app/services/language/language.service';
+import { SwiperOptions } from 'swiper';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,12 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-order-details.page.scss'],
 })
 export class MyOrderDetailsPage implements OnInit {
+  orderConfig: SwiperOptions;
+  currentlangauge: string;
+  orderImages: any = [
+    './../../../../assets/images/projects/img1.svg',
+    './../../../../assets/images/projects/img2.svg',
+    './../../../../assets/images/projects/img3.svg',
+    './../../../../assets/images/projects/img1.svg',
+    './../../../../assets/images/projects/img3.svg',
+  ];
   orderDetails: any = {
     id: 1,
     status: 1,
     requestStatus: 'waiting',
     requestNumber: 3288372,
     date: 'الخميس12 مارس 2021',
+    cleintName: 'محمد احمد',
+    city: 'الرياض',
+    neighborhood: 'حي الملز',
   };
   requestItems: any = [
     {
@@ -30,19 +44,21 @@ export class MyOrderDetailsPage implements OnInit {
 
   clientData: any = [
     {
-      text: 'orderNumber',
-      value: '#' + this.orderDetails.requestNumber,
+      text: 'cleint-name',
+      value: this.orderDetails.cleintName,
     },
     {
-      text: 'orderStatus',
-      value: this.orderDetails.requestStatus,
+      text: 'city',
+      value: this.orderDetails.city,
     },
     {
-      text: 'orderStatus',
-      value: this.orderDetails.date,
+      text: 'the area or neighborhood',
+      value: this.orderDetails.neighborhood,
     },
   ];
-  constructor() {}
+  constructor(private LanguageService: LanguageService) {
+    this.currentlangauge = this.LanguageService.getLanguage();
+  }
 
   ngOnInit() {}
 }
