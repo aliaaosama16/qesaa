84ce104d6b7ee9ae4950a0645c988094e3cc541c
akram yasser <akramkoyasser@gmail.com>
2022-05-09 10:19:29 +0200

    order

--- src/app/pages/tabs-pages/my-order-details/my-order-details.page.ts
+++ src/app/pages/tabs-pages/my-order-details/my-order-details.page.ts
@@ -1,5 +1,3 @@
-import { LanguageService } from 'src/app/services/language/language.service';
-import { SwiperOptions } from 'swiper';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -8,62 +6,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-order-details.page.scss'],
 })
 export class MyOrderDetailsPage implements OnInit {
-
-  orderConfig:SwiperOptions;
-  currentlangauge :string;
-  orderImages:any=[
-    './../../../../assets/images/projects/img2.svg',
-    './../../../../assets/images/projects/img1.svg',
-    './../../../../assets/images/projects/img2.svg',
-    './../../../../assets/images/projects/img3.svg'
-  ]
-  orderDetails:any={
-    id:1,
-    status:1,
-    requestStatus:'waiting',
-    requestNumber:3288372,
-    date:'الخميس12 مارس 2021'
+  orderDetails: any = {
+    id: 1,
+    status: 1,
+    requestStatus: 'waiting',
+    requestNumber: 3288372,
+    date: 'الخميس12 مارس 2021',
   };
-  requestItems:any=[
+  requestItems: any = [
     {
-      text:"orderNumber",
-      value:'#'+this.orderDetails.requestNumber
+      text: 'orderNumber',
+      value: '#' + this.orderDetails.requestNumber,
     },
     {
-      text:"orderStatus",
-      value:this.orderDetails.requestStatus
+      text: 'orderStatus',
+      value: this.orderDetails.requestStatus,
     },
     {
-      text:"orderStatus",
-      value:this.orderDetails.date
-    }
+      text: 'orderStatus',
+      value: this.orderDetails.date,
+    },
   ];
 
-  clientData:any=[
+  clientData: any = [
     {
-      text:"cleint-name",
-      value:"محمد احمد"
+      text: 'orderNumber',
+      value: '#' + this.orderDetails.requestNumber,
     },
     {
-      text:"city",
-      value:"الرياض"
+      text: 'orderStatus',
+      value: this.orderDetails.requestStatus,
     },
     {
-      text:"the area or neighborhood",
-      value:"حي الملز"
-    }
+      text: 'orderStatus',
+      value: this.orderDetails.date,
+    },
   ];
-  constructor(private languageService:LanguageService) { 
-    this.orderConfig= {
-      slidesPerView: 3,
-      spaceBetween: 11,
-      pagination: false,
-      effect: 'fade',
-    };
-    this.currentlangauge=this.languageService.getLanguage();
-  }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
